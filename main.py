eb0f9837e94956cf27b01a58bc92cd68cf4f5099
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-04-15 12:30:35 -0500

    GET POST HEAD

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import handler
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8000
 
+#AF_INET == IPv6 Socket type Sock_Stream (Streams using TCP transportation protocol)
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind((SERVER_HOST, SERVER_PORT))
+serverSocket.bind((SERVER_HOST, SERVER_PORT))#Socket parameters (HOSTIP,PORT)
 serverSocket.listen(1)
+#defines how many unaccepted devices the server allows
+#it specifies the number of unaccepted connections that the system will allow before refusing new connections
 print('ACTIVE PORT => %s' % SERVER_PORT)
 
 while True:    
     clientConnection, clientAddress = serverSocket.accept()
-
+    #Threading 
     thread = threading.Thread(target= handler.handleClient, args=(clientConnection, clientAddress))
     thread.start()
 
