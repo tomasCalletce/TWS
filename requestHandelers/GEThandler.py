1ecb717f8d09b61130ea18c56818605333369cf8
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-04-15 15:59:02 -0500

    finish server

--- requestHandelers/GEThandler.py
+++ requestHandelers/GEThandler.py
@@ -16,6 +16,20 @@ def getHandle(arr):
 
         response = headers + page
         return  response.encode('utf-8')
+    elif arr[1] == '/page_three':
+        page  = getFile('pageThree.html',"r")
+
+        headers= f"HTTP/1.1 200 OK\r\nContent-Length: {len(page)}\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: close\r\n\r\n"
+
+        response = headers + page
+        return  response.encode('utf-8')
+    elif arr[1] == '/page_four':
+        page  = getFile('pageFour.html',"r")
+
+        headers= f"HTTP/1.1 200 OK\r\nContent-Length: {len(page)}\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: close\r\n\r\n"
+
+        response = headers + page
+        return  response.encode('utf-8')
 
     elif arr[1] == '/image_user_one':
         image  = getFile('user1.jpg',"rb")
@@ -29,6 +43,12 @@ def getHandle(arr):
         headers= f"HTTP/1.1 200 OK\r\nContent-Length: {len(image)}\r\nContent-Type: image/jpeg\r\nConnection: close\r\n\r\n"
 
         return  headers.encode('utf-8') + image
+    elif arr[1] == '/big_file':
+        textFile  = getFile('bigFile.txt',"rb")
+
+        headers= f"HTTP/1.1 200 OK\r\nContent-Length: {len(textFile)}\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Disposition: attachment; filename=large_file.txt\r\nConnection: close\r\n\r\n"
+
+        return  headers.encode('utf-8') + textFile
     else:
         headers= 'HTTP/1.0 404 Page/File Not Found\n\n'
         return headers.encode('utf-8')
