4e60ed209b9bd3a405d1e21bfdd75abe708b85d1
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-04-15 15:24:58 -0500

    finish head request

--- requestHandelers/HEADhandler.py
+++ requestHandelers/HEADhandler.py
@@ -1,7 +1,14 @@
+from utils.main import getFile
+
 def headHandle(arr):
     if arr[1] == '/image_user_one':
-        header = 'HTTP/1.0 200 Page/File Not Found\n\n'
+        file = getFile('user1.jpg',"rb")
+        header = f"HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: {len(file)}\r\nConnection: close\r\n\r\n"
+        return header.encode('utf-8')
+    elif arr[1] == '/image_user_two':
+        file = getFile('user2.jpg',"rb")
+        header = f"HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: {len(file)}\r\nConnection: close\r\n\r\n"
         return header.encode('utf-8')
     else:
-        header = 'HTTP/1.0 400 Bad Request\n\n'
+        header = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 0\r\nConnection: close\r\n\r\n"
         return header.encode('utf-8')
